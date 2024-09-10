4eeaa392ba43a64e8d5280b2a365f7a594d1bce8
valentinaromans <valentina.roman03@inacapmail.cl>
2024-09-10 01:21:16 -0300

    parte final

--- roman/views.py
+++ roman/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def danitza(request):
+    html = """
+    <h1>Hola, Danitza</h1>
+    """
+    return HttpResponse(html)
+
+def sepulveda(request):
+    html = """
+    <h2>Hola, Sepúlveda</h2>
+    """
+    return HttpResponse(html)
